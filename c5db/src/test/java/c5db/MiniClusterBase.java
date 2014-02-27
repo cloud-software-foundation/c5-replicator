ebac401448ecf3fb11acb6fe24081351a64e1786
Alex Newman <posix4e@apache.org>
2014-02-27 11:31:55 -0800

    Reduced websocket handshake complexity.

--- c5db/src/test/java/c5db/MiniClusterBase.java
+++ c5db/src/test/java/c5db/MiniClusterBase.java
@@ -16,7 +16,6 @@
  */
 package c5db;
 
-import c5db.C5DB;
 import c5db.interfaces.C5Module;
 import c5db.interfaces.C5Server;
 import c5db.interfaces.ReplicationModule;
@@ -27,75 +26,65 @@ import org.jetlang.channels.Channel;
 import org.jetlang.core.Callback;
 import org.jetlang.fibers.Fiber;
 import org.jetlang.fibers.ThreadFiber;
-import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
 import org.mortbay.log.Log;
 
-import java.io.IOException;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 public class MiniClusterBase {
-    private static int regionServerPort;
-    static boolean initialized = false;
-    private static Random rnd = new Random();
+  static boolean initialized = false;
+  private static int regionServerPort;
+  private static Random rnd = new Random();
 
-    public static int getRegionServerPort() throws InterruptedException {
-        while (initialized = false) {
-            Thread.sleep(100);
-        }
-        return regionServerPort;
+  public static int getRegionServerPort() throws InterruptedException {
+    return regionServerPort;
+  }
+
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    Log.warn("-----------------------------------------------------------------------------------------------------------");
+
+
+    regionServerPort = 8080 + rnd.nextInt(1000);
+    System.setProperty("singleNode", "true");
+    System.setProperty("regionServerPort", String.valueOf(regionServerPort));
+
+    C5DB.main(new String[]{});
+    C5Server server = C5DB.getServer();
+
+    ListenableFuture<C5Module> regionServerFuture = server.getModule(ModuleType.RegionServer);
+    C5Module regionServer = regionServerFuture.get();
+
+    ListenableFuture<C5Module> tabletServerFuture = server.getModule(ModuleType.Tablet);
+    TabletModule tabletServer = (TabletModule) tabletServerFuture.get();
+
+    ListenableFuture<C5Module> replicationServerFuture = server.getModule(ModuleType.Replication);
+    ReplicationModule replicationServer = (ReplicationModule) replicationServerFuture.get();
+
+    while (!regionServer.isRunning() ||
+        !tabletServer.isRunning() ||
+        !replicationServer.isRunning()) {
+      Thread.sleep(600);
     }
 
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        Log.warn("-----------------------------------------------------------------------------------------------------------");
+    Channel<TabletModule.TabletStateChange> stateChanges = tabletServer.getTabletStateChanges();
 
+    Fiber receiver = new ThreadFiber();
+    receiver.start();
 
-        regionServerPort = 8080 + rnd.nextInt(1000);
-        System.setProperty("singleNode", "true");
-        System.setProperty("regionServerPort", String.valueOf(regionServerPort));
+    // create java.util.concurrent.CountDownLatch to notify when message arrives
+    final CountDownLatch latch = new CountDownLatch(1);
 
-        C5DB.main(new String[]{});
-        C5Server server = C5DB.getServer();
-
-        ListenableFuture<C5Module> regionServerFuture = server.getModule(ModuleType.RegionServer);
-        C5Module regionServer = regionServerFuture.get();
-
-        ListenableFuture<C5Module> tabletServerFuture = server.getModule(ModuleType.Tablet);
-        TabletModule tabletServer = (TabletModule) tabletServerFuture.get();
-
-        ListenableFuture<C5Module> replicationServerFuture = server.getModule(ModuleType.Replication);
-        ReplicationModule replicationServer = (ReplicationModule) replicationServerFuture.get();
-
-        while (!regionServer.isRunning() ||
-                !tabletServer.isRunning() ||
-                !replicationServer.isRunning()) {
-            Thread.sleep(600);
-        }
-
-        Channel<TabletModule.TabletStateChange> stateChanges = tabletServer.getTabletStateChanges();
-
-        Fiber receiver = new ThreadFiber();
-        receiver.start();
-
-        // create java.util.concurrent.CountDownLatch to notify when message arrives
-        final CountDownLatch latch = new CountDownLatch(1);
-
-        Callback<TabletModule.TabletStateChange> onMsg = new Callback<TabletModule.TabletStateChange>() {
-            public void onMessage(TabletModule.TabletStateChange message) {
-                //open latch
-                System.out.println(message);
-                initialized = true;
-                latch.countDown();
-            }
-        };
-
-        stateChanges.subscribe(receiver, onMsg);
-
-        while (initialized == false) {
-            Thread.sleep(100);
-        }
-    }
+    Callback<TabletModule.TabletStateChange> onMsg = new Callback<TabletModule.TabletStateChange>() {
+      public void onMessage(TabletModule.TabletStateChange message) {
+        //open latch
+        System.out.println(message);
+        initialized = true;
+        latch.countDown();
+      }
+    };
+    stateChanges.subscribe(receiver, onMsg);
+    latch.await();
+  }
 }
