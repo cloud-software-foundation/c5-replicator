b1647d2aa865f11f7b76b083c2a9358d1fabcde9
Joshua Greenberg <josh@ohmdata.com>
2014-07-20 20:36:52 -0400

    Stub out C5GeneralizedReplicationService

---
+++ c5-general-replication/src/main/java/c5db/replication/C5GeneralizedReplicationService.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2014  Ohm Data
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package c5db.replication;
+
+import c5db.SimpleC5ModuleServer;
+import c5db.interfaces.DiscoveryModule;
+import c5db.interfaces.LogModule;
+import c5db.interfaces.ReplicationModule;
+import c5db.interfaces.replication.Replicator;
+import c5db.log.LogService;
+import c5db.util.C5Futures;
+import com.google.common.util.concurrent.AbstractService;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.Service;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.jetlang.core.Disposable;
+import org.jetlang.fibers.Fiber;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+
+import static com.google.common.util.concurrent.Futures.allAsList;
+
+
+/**
+ * Replication server implementing a variation of the Raft algorithm; internally
+ * it bundles a ReplicationModule, a LogModule, and a DiscoveryModule. It then implements
+ * the ReplicationModule interface and delegates to its internal ReplicationModule.
+ */
+public class C5GeneralizedReplicationService extends AbstractService {
+  private static final int NUMBER_OF_PROCESSORS = Runtime.getRuntime().availableProcessors();
+
+  private final ReplicatorService.FiberFactory fiberFactory;
+  private final Fiber serverFiber;
+  private final SimpleC5ModuleServer moduleServer;
+  private final DiscoveryModule nodeInfoModule;
+  private final LogModule logModule;
+  private final ReplicationModule replicationModule;
+
+  private final List<Disposable> disposables = new ArrayList<>();
+
+  private final EventLoopGroup bossGroup = new NioEventLoopGroup(NUMBER_OF_PROCESSORS / 3);
+  private final EventLoopGroup workerGroup = new NioEventLoopGroup(NUMBER_OF_PROCESSORS / 3);
+
+  public C5GeneralizedReplicationService(
+      Path basePath,
+      long nodeId,
+      int replicatorPort,
+      DiscoveryModule nodeInfoModule,
+      ReplicatorService.FiberFactory fiberFactory) {
+
+    this.nodeInfoModule = nodeInfoModule;
+    this.fiberFactory = fiberFactory;
+
+    serverFiber = fiberFactory.getFiber(this::notifyFailed);
+    moduleServer = new SimpleC5ModuleServer(serverFiber);
+    serverFiber.start();
+
+    replicationModule = new ReplicatorService(bossGroup, workerGroup, nodeId, replicatorPort, moduleServer,
+        fiberFactory, new NioQuorumFileReaderWriter(basePath));
+
+    logModule = new LogService(basePath);
+  }
+
+  @Override
+  protected void doStart() {
+    List<ListenableFuture<Service.State>> startFutures = new ArrayList<>();
+
+    startFutures.add(moduleServer.startModule(logModule));
+    startFutures.add(moduleServer.startModule(nodeInfoModule));
+    startFutures.add(moduleServer.startModule(replicationModule));
+
+    ListenableFuture<List<Service.State>> allFutures = allAsList(startFutures);
+
+    C5Futures.addCallback(allFutures,
+        (List<Service.State> ignore) -> notifyStarted(),
+        this::notifyFailed,
+        serverFiber);
+  }
+
+  @Override
+  protected void doStop() {
+    replicationModule.stopAndWait();
+    nodeInfoModule.stopAndWait();
+    logModule.stopAndWait();
+
+    notifyStopped();
+  }
+
+  public ListenableFuture<GeneralizedReplicator> createReplicator(String quorumId, Collection<Long> peerIds) {
+    // TODO the failure of the fiber passed to C5GeneralizedReplicator should not fail this entire service.
+    return Futures.transform(
+        replicationModule.createReplicator(quorumId, peerIds),
+        (Replicator replicator) ->
+            new C5GeneralizedReplicator(replicator, createAndStartFiber(this::notifyFailed)));
+  }
+
+  public void dispose() {
+    serverFiber.dispose();
+    disposables.forEach(Disposable::dispose);
+  }
+
+  private Fiber createAndStartFiber(Consumer<Throwable> throwableHandler) {
+    Fiber newFiber = fiberFactory.getFiber(throwableHandler);
+    serverFiber.execute(() -> disposables.add(newFiber));
+    newFiber.start();
+    return newFiber;
+  }
+}
+
